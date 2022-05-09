91b99c600c0c1b59a18781d7c12e76ce0ef0776c
MiyaMeiMei <zdmnupt@163.com>
2022-05-09 20:06:48 +0800

    commit 5/9 for get data from csv

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './component/App';
 // import piecharts from './component/Piecharts';
 import reportWebVitals from './reportWebVitals';
+import Papa from 'papaparse';
+import $ from 'jquery';
+// import DATA from "./data/releasejira.csv"
+
+// $.get("./data/releasejira.csv",function(data){
+//   console.log(data);
+// }, "json");
+async function getSrouceData(){
+  const response = await fetch("./data/releasejira.csv");
+  let reader = response.body.getReader();
+  let decoder = new TextDecoder('utf-8');
+  let result = await reader.read();
+  let data = await decoder.decode(result.value);
+  let results = Papa.parse(await data, {header: true});
+  let rows = results.data
+  console.log(rows);
+}
+
+getSrouceData();
 
 ReactDOM.render(
   <React.StrictMode>
